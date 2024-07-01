82d4fa7aa60c8dc5ffdee5d787fb273ffed6339c
David Alakhume <98941567+Davalak@users.noreply.github.com>
2024-07-01 03:36:23 +0000

    Added failed attempt sessions and redirect

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php 
+session_start();
+
+
+?>
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -8,7 +14,10 @@
     
     <h1> Assignment 1 </h1>
 
+    <p> Welcome, <?=$_SESSION['username'] ?> </p>
+
     <p><a href="/login.php">Click here to login</p>
+
 
 
     
--- validate.php
+++ validate.php
@@ -1,14 +1,27 @@
 <?php
 
+session_start();
+
   $valid_username = "david";
   $valid_password = "password";
 
   $username = $_REQUEST['username'];
+
+  $_SESSION['username'] = $username;
+
   $password = $_REQUEST['password'];
 
   if ($username == $valid_username && $password == $valid_password) {
-    echo "Welcome $username";
+    header("Location: /index.php");
   }
-  else {echo "Invalid username or password";}
+  else {
+    if (!isset($_SESSION['failed_attempts'])){
+      $_SESSION['failed_attempts'] = 1;
+    }
+    else {
+      $_SESSION['failed_attempts']++;
+    }
+      echo "This is unsuccesful attempt #" . $_SESSION['failed_attempts'];
+      }
 
 ?>
